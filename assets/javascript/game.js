307d35afa208ab7c1c10e825a8793831202af695
Miles Kenner <mdk0109@yahoo.com>
2018-12-18 15:46:38 -0500

    Here's some pseudo code in Javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Have the computer generate a random number between 19 and 120. 
+
+// Display that number in the provided box. 
+
+// Have the wins and losses display in the box. 
+
+// Have each button be assigned a random value between 1 and 12 and change between each round. 
+
+// Have click functions assigned to each button to add the present value to the total score. 
+
+// Set conditions for wins and losses. If the total score is over the computerGuess then the round is over and it counts as a loss (losses--;). 
+
+// If the player score is exactly matched to the computerGuess then the player wins (wins++;).
+
+// Once the player reaches 5 wins or 5 losses then the game is over and everything resets. 
