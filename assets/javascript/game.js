e5a2a0315d6663b08c5ef7e58ffedb3719efdbda
Miles Kenner <mdk0109@yahoo.com>
2018-12-20 15:06:29 -0500

    final finishes to the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,8 @@ var rubyGuess = Math.floor(Math.random() * 12) +1;
 var sapphireGuess = Math.floor(Math.random() * 12) +1;
 var crystalGuess = Math.floor(Math.random() * 12) +1; 
 var emeraldGuess = Math.floor(Math.random() * 12) +1; 
+
+// Create variables for the user total, wins and losses. Set them to 0 so that they can be added to. 
 var userTotal = 0; 
 var wins = 0;
 var losses = 0; 
@@ -19,51 +21,50 @@ console.log("Losses: " + losses);
 
 $(document).ready(function(){
 
-//  restartGame (function() {
-//     computerGuess = Math.floor(Math.random() * 120) +19;
-//     rubyGuess = Math.floor(Math.random() * 12) +1;
-//     sapphireGuess = Math.floor(Math.random() * 12) +1;
-//     crystalGuess = Math.floor(Math.random() * 12) +1;
-//     emeraldGuess = Math.floor(Math.random() * 12) +1;        
-//     userTotal = 0;
-//     wins = 0;
-//     losses = 0;
-
-// console.log(rubyGuess);
-// console.log(sapphireGuess);
-// console.log(crystalGuess);
-// console.log(emeraldGuess); 
-// console.log(userTotal); 
-// console.log("Wins: " + wins);
-// console.log("Losses: " + losses);
-//     });
-
-// Display that number in their provided box. 
+// Display the random number in the provided box. 
 $(".random-number").html(computerGuess); 
 console.log(computerGuess);
 
-// Have the wins and losses display in the box. 
+// Have the wins and losses display in the box as well as the user total.  
 $("#wins").html("Wins: " + wins);
 $("#losses").html("Losses: " + losses);
 $(".points-so-far").html(userTotal);
 
 // Have click functions assigned to each button to add the present value to the total score. 
 $("#ruby").on("click", function() {
+
+    // the user total adds the current value of each button and displays that. 
     userTotal = userTotal + rubyGuess; 
+
+    // Display new score. 
     $(".points-so-far").html(userTotal);
+
+    // Always check to make sure stuff is working. 
     console.log(userTotal, computerGuess);
+
+    // If the user total is greater that the computer's guess then it counts as a loss. 
     if (userTotal > computerGuess) {
+
+        // New value of Losses increases by one. 
         losses++; 
+
+        // set the user total back to 0 
         userTotal = 0 
+
+        // Choose new random values for the computerChoice and each button. 
         computerGuess = Math.floor(Math.random() * 120) +19;
         rubyGuess = Math.floor(Math.random() * 12) +1;
         sapphireGuess = Math.floor(Math.random() * 12) +1;
         crystalGuess = Math.floor(Math.random() * 12) +1;
         emeraldGuess = Math.floor(Math.random() * 12) +1;
+
+        // Display new scores, values and totals. 
         $(".random-number").html(computerGuess); 
         $("#wins").html("Wins: " + wins);
         $("#losses").html("Losses: " + losses);
         $(".points-so-far").html(userTotal);
+
+        // Make sure everything is working. 
         console.log("You lost this round");
         console.log(rubyGuess);
         console.log(sapphireGuess);
@@ -73,18 +74,29 @@ $("#ruby").on("click", function() {
         console.log("Wins: " + wins);
         console.log("Losses: " + losses);
     }
+
+    // If player matches the computerGuess then count it as a win and reset everything. 
     if (userTotal === computerGuess) {
+        // wins' value goes up by one 
         wins++; 
+        
+        //Reset user total to 0. 
         userTotal = 0 
+
+        // Reset the random values for the computerGuess and the buttons. 
         computerGuess = Math.floor(Math.random() * 120) +19;
         rubyGuess = Math.floor(Math.random() * 12) +1;
         sapphireGuess = Math.floor(Math.random() * 12) +1;
         crystalGuess = Math.floor(Math.random() * 12) +1;
         emeraldGuess = Math.floor(Math.random() * 12) +1;
+
+        // Display the new values, scores and totals
         $(".random-number").html(computerGuess); 
         $("#wins").html("Wins: " + wins);
         $("#losses").html("Losses: " + losses);
         $(".points-so-far").html(userTotal);
+
+        // Check and make sure everything is reset. 
         console.log("You win this time");
         console.log(rubyGuess);
         console.log(sapphireGuess);
@@ -95,6 +107,24 @@ $("#ruby").on("click", function() {
         console.log("Losses: " + losses);
     }
     
+    // Once the player reaches 5 wins or 5 losses then the game is over and everything resets. 
+if (wins === 5) {
+    alert("You have won the game!")
+    wins = 0; 
+    losses = 0; 
+    $("#wins").html("Wins: " + wins);
+    $("#losses").html("Losses: " + losses);
+}
+
+if (losses === 5) {
+    alert("You have lost the game")
+    wins = 0;
+    losses = 0;
+    $("#wins").html("Wins: " + wins);
+        $("#losses").html("Losses: " + losses);
+}
+
+
 });
 
 $("#sapphire").on("click", function() {
@@ -143,6 +173,24 @@ $("#sapphire").on("click", function() {
         console.log("Wins: " + wins);
         console.log("Losses: " + losses);
     }
+
+    // Once the player reaches 5 wins or 5 losses then the game is over and everything resets. 
+if (wins === 5) {
+    alert("You have won the game!")
+    wins = 0; 
+    losses = 0; 
+    $("#wins").html("Wins: " + wins);
+        $("#losses").html("Losses: " + losses);
+}
+
+if (losses === 5) {
+    alert("You have lost the game")
+    wins = 0;
+    losses = 0; 
+    $("#wins").html("Wins: " + wins);
+        $("#losses").html("Losses: " + losses);
+}
+
 });
 
 $("#crystal").on("click", function() {
@@ -191,6 +239,24 @@ $("#crystal").on("click", function() {
         console.log("Wins: " + wins);
         console.log("Losses: " + losses);
     }
+
+    // Once the player reaches 5 wins or 5 losses then the game is over and everything resets. 
+if (wins === 5) {
+    alert("You have won the game!")
+    wins = 0; 
+    losses = 0; 
+    $("#wins").html("Wins: " + wins);
+        $("#losses").html("Losses: " + losses);
+}
+
+if (losses === 5) {
+    alert("You have lost the game")
+    wins = 0;
+    losses = 0; 
+    $("#wins").html("Wins: " + wins);
+        $("#losses").html("Losses: " + losses);
+}
+
 });
 
 $("#emerald").on("click", function() {
@@ -239,20 +305,24 @@ $("#emerald").on("click", function() {
         console.log("Wins: " + wins);
         console.log("Losses: " + losses);
     }
-});
-
-// Once the player reaches 5 wins or 5 losses then the game is over and everything resets. 
-if (wins > 5) {
+    // Once the player reaches 5 wins or 5 losses then the game is over and everything resets. 
+if (wins === 5) {
     alert("You have won the game!")
     wins = 0; 
     losses = 0; 
+    $("#wins").html("Wins: " + wins);
+        $("#losses").html("Losses: " + losses);
 }
 
-if (losses > 5) {
+if (losses === 5) {
     alert("You have lost the game")
     wins = 0;
     losses = 0; 
+    $("#wins").html("Wins: " + wins);
+        $("#losses").html("Losses: " + losses);
 }
+
+});
 
 
 });
